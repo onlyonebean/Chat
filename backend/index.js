eab482dd35e70063c186bd8331849bf44924fa30
onlyonebean <146683948+onlyonebean@users.noreply.github.com>
2023-10-10 20:19:04 -0700

    Clean up 2

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express"); //import express-runs the HTTP server;cors-to call the server from any other origin
 const cors = require("cors");
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,15 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+  try {
+    const r = await axios.put(
+        "https://api.chatengine.io/users/",
+        {username: username, secret: username, first_name: username},
+        {headers: {"private-key": "bd9d6a98-bb6b-4c55-9372-e4a133aa4834"}}
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
-
 app.listen(3001);
